467e720950d6edc5f977cb9e41057846840bda3f
CPPGhettoIrony <manolitor2003@gmail.com>
2025-11-19 13:05:55 +0100

    main

---
+++ grafo.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "grafo.h"
+
+grafo crear_grafo(int num_nodos){
+    grafo ret;
+    ret.num_nodos = num_nodos;
+    ret.coste = (int**)malloc(num_nodos * sizeof(int*));
+    for(int i = 0; i < num_nodos; ++i)
+        ret.coste[i] = (int*)malloc(num_nodos * sizeof(int));
+    return ret;
+}
+
+void liberar_grafo(grafo* g) {
+    for(int i = 0; i < g->num_nodos; ++i)
+        free(g->coste[i]); 
+    free(g->coste);
+}
---
+++ grafo.h
@@ -0,0 +1,10 @@
+typedef struct {
+    int** coste, num_nodos;
+} grafo;
+
+// Crea un grafo con (num_nodos) nodos
+grafo crear_grafo(int num_nodos);
+
+// Libera la memoria asociada con los nodos
+void liberar_grafo(grafo* g);
+
