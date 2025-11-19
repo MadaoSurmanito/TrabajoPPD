01f27da90948ceb784ad555d355a1899e6479725
CPPGhettoIrony <manolitor2003@gmail.com>
2025-11-19 13:46:19 +0100

    man

--- evaluar.c
+++ evaluar.c
@@ -1,13 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "evaluar.h"
 #include "grafo.h"
 
 int evaluar(int* individuo, grafo* g) {
+
+    unsigned char* repetidos = malloc(g->num_nodos);
+    memset(repetidos, 0, g->num_nodos);
+
     int ret = 0;
-    for(int i = 0; i < g->num_nodos - 1; ++i) {
-        int coste = g->coste[individuo[i]][individuo[i+1]];
+
+    for(int i = 0; i < g->num_nodos; ++i) {
+        
+        // Se comprueba si ya se ha pasado por ese nodo
+        if(repetidos[i])
+            return -1;
+        repetidos[i] = 1;
+
+        // Se obtiene el coste de ir de un nodo al siguiente en el camino
+        int coste = g->coste[individuo[i]][individuo[(i+1)%g->num_nodos]];
         if(coste < 0)
             return -1;
         ret += coste;
-    }
+    }  
+
     return ret;
 }
---
+++ poblacion.h
@@ -0,0 +1,9 @@
+typedef struct {
+    int** individuos, num_individuos, num_nodos;
+} poblacion;
+
+// Crea una población con (num_individuos) individuos, cada uno es una lista de (num_nodos) nodos del camino
+poblacion crear_poblacion(int num_individuos, int num_nodos);
+
+// Libera la memoria asociada con la población
+void liberar_poblacion(poblacion* g);
