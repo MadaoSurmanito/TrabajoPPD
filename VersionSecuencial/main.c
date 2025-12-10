e0dac85d9bc29ce89e9f94987d53d8b49576c7dc
MadaoSurmanito <alejandro.sanzhuerta@alum.uca.es>
2025-12-10 13:45:52 +0100

    En esta version FUNCIONAN los main de Version Secuencial y Version Secuencial Izhikevich

--- VersionSecuencial/main.c
+++ VersionSecuencial/main.c
@@ -15,7 +15,7 @@ int main()
 {
     printf("Algoritmo Genético para el Ciclo Hamiltoniano\n");
     printf("=============================================\n");
-    int ngens = 10000, TPoblacion = 20; // ngens = numero de generaciones     TPoblacion = tamaño poblacion
+    int ngens = 2000000, TPoblacion = 20; // ngens = numero de generaciones     TPoblacion = tamaño poblacion
 
     printf("Cargando grafo desde archivo...\n");
     grafo MCostes = cargar_grafo("../Pruebas/eil51.tsp");
@@ -59,19 +59,10 @@ int *algGen_CHamiltoniano(int ngens, int TPoblacion, grafo *MCostes)
     {
         int madre[MCostes->num_nodos], padre[MCostes->num_nodos], *hijo;
         printf("Generación %d/%d\n", i + 1, ngens);
-        printf("---------------------\n");
-        printf("Ejecutando emparejamiento...\n");
         emparejamiento(pob, padre, madre, MCostes->num_nodos); // Devvuelve 2 individuos(padre y madre)
-        printf("---------------------\n");
-        printf("Ejecutando cruce...\n");
         hijo = cruce(padre, madre, MCostes); // Crea un nuevo individuo válido(hijo)
-        printf("---------------------\n");
-        printf("Ejecutando mutación...\n");
         mutacion(hijo, MCostes->num_nodos); // Modifica al hijo dentro de una probabilidad
-        printf("---------------------\n");
-        printf("Ejecutando selección...\n");
         seleccion(&pob, MCostes, hijo); // Intenta insertar al hijo en la población
-        printf("---------------------\n");
         
         if (CosteMejorSolucion != -1 && CosteMejorSolucion > evaluar(hijo, MCostes))
         {
